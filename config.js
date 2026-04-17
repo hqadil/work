f1c32ffc73972a10aaf56424dacbb274038bfbf9
hqadil <hqadil6@gmail.com>
2026-04-17 13:49:50 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,5 +4,5 @@
 // Settings → API → Project URL & anon/public key
 // ============================================================
 
-const SUPABASE_URL = 'YOUR_SUPABASE_PROJECT_URL';
-const SUPABASE_ANON_KEY = 'YOUR_SUPABASE_ANON_KEY';
+const SUPABASE_URL = 'https://rmirpbatguppzlgodfwg.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJtaXJwYmF0Z3VwcHpsZ29kZndnIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzY0MDkxNTUsImV4cCI6MjA5MTk4NTE1NX0.xOGziyIekqXmJRcrZJbVmugcAqjMBeGPgxwd4zcdZZE';
